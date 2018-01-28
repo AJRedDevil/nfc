0a8506e37237571870ee78227c14e4267ca91bf0
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2018-01-28 11:32:26 +0545

    added the json options for list of urls

--- server/util/RESTHelper.js
+++ server/util/RESTHelper.js
@@ -1,7 +1,10 @@
-const getJSONOptions = uri => ({
+const getApplyJSONOptions = uri => ({
   uri,
   json: true,
 });
+
+const getJSONOptions = uri =>
+  Array.isArray(uri) ? uri.map(getApplyJSONOptions) : getApplyJSONOptions(uri);
 
 module.exports = {
   getJSONOptions,
--- server/util/__test__/RESTHelper.spec.js
+++ server/util/__test__/RESTHelper.spec.js
@@ -1,11 +1,29 @@
 const {getJSONOptions} = require('../RESTHelper');
 
-test('Test correct GET request', () => {
-  const testURI = 'http://example.com';
-  const response = getJSONOptions(testURI);
-  const expected = {
-    uri: testURI,
-    json: true,
-  };
-  expect(response).toEqual(expected);
+describe('RESTHelper', () => {
+  test('Test for single GET request', () => {
+    const testURI = 'http://example.com';
+    const response = getJSONOptions(testURI);
+    const expected = {
+      uri: testURI,
+      json: true,
+    };
+    expect(response).toEqual(expected);
+  });
+
+  test('Test list of GET request', () => {
+    const testURI = ['http://example.com', 'http://test.com'];
+    const response = getJSONOptions(testURI);
+    const expected = [
+      {
+        uri: testURI[0],
+        json: true,
+      },
+      {
+        uri: testURI[1],
+        json: true,
+      },
+    ];
+    expect(response).toEqual(expected);
+  });
 });
