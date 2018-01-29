3fcb1b80eb1ca2af439ec327c15c32fbfc61f07e
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2018-01-29 21:59:07 +0545

    update Home scene

--- client/src/scenes/Home/index.js
+++ client/src/scenes/Home/index.js
@@ -1,3 +1,30 @@
-import ClassicTable from './components/ClassicTable';
+// npm packages
+import React, {Component} from 'react';
 
-export default ClassicTable;
+// our packages
+import ClassicTable from './components/ClassicTable';
+import classicLeagueAPI from '../../services/api';
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'NFC Classic',
+      subtitle: 'Top 3',
+      data: {},
+    };
+  }
+
+  componentDidMount() {
+    classicLeagueAPI
+      .getTop3()
+      .then(data => this.setState({data}))
+      .catch(error => console.error(error));
+  }
+
+  render() {
+    return <ClassicTable {...this.state} />;
+  }
+}
+
+export default Home;
