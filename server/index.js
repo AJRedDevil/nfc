9ae9446ad780991c7243faf8917e181ac5e9fde1
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2018-01-29 06:02:25 +0545

    move the server start code into index.js

---
+++ server/index.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+// Require the framework and instantiate it
+const fastify = require('fastify')();
+
+// Register route
+fastify.register(require('./routes'));
+
+// Run the server!
+const start = async () => {
+  try {
+    await fastify.listen(3000);
+    fastify.log.info(`server listening on ${fastify.server.address().port}`);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
