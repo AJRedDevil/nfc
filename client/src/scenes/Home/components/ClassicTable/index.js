698393017453064ce93f125108c9e1e1cb7db3de
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2018-01-29 21:57:45 +0545

    update ClassicTable with TableHeading

--- client/src/scenes/Home/components/ClassicTable/index.js
+++ client/src/scenes/Home/components/ClassicTable/index.js
@@ -1,9 +1,24 @@
 // npm packages
 import React from 'react';
+import {shape, string} from 'prop-types';
 
 // our packages
+import TableHeading from '../../../../components/TableHeading';
 import Table from '../../../../components/Table';
+import ClassicPropType from '../../../../components/Table/propTypes';
 
-const ClassicTable = props => <Table {...props} />;
+const ClassicTable = props => (
+  <div>
+    <TableHeading {...props} />
+    <Table {...props.data} />
+  </div>
+);
+ClassicTable.propTypes = {
+  data: shape({
+    head: ClassicPropType.tableHead,
+    body: ClassicPropType.tableBody,
+    foot: ClassicPropType.tableFoot,
+  }),
+};
 
 export default ClassicTable;
