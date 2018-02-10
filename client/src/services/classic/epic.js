fd8897fa538a632b0abf76aa9533c8ebc2563880
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2018-02-10 13:25:37 +0545

    Add errorhandler for classic standings

--- client/src/services/classic/epic.js
+++ client/src/services/classic/epic.js
@@ -1,12 +1,22 @@
+// npm packages
+import {Observable} from 'rxjs';
+
 // our packages
 import {classicDataFetched} from './actions';
-import {FETCH_DATA} from './actionTypes';
+import {FETCH_DATA, FETCH_DATA_REJECTED} from './actionTypes';
 import {classicLeagueAPI} from '../api';
 
 const fetchClassicStandingsEpic = action$ =>
   action$
     .ofType(FETCH_DATA)
     .mergeMap(() => classicLeagueAPI.getTop3())
-    .map(response => classicDataFetched(response));
+    .map(response => classicDataFetched(response))
+    .catch(error =>
+      Observable.of({
+        type: FETCH_DATA_REJECTED,
+        payload: error.xhr.response,
+        error: true,
+      })
+    );
 
 export default {fetchClassicStandingsEpic};
--- client/src/services/classic/reducer.js
+++ client/src/services/classic/reducer.js
@@ -1,4 +1,4 @@
-import {FETCH_DATA, DATA_FETCHED} from './actionTypes';
+import {FETCH_DATA, FETCH_DATA_REJECTED, DATA_FETCHED} from './actionTypes';
 
 const intialState = {
   title: 'NFC Classic',
@@ -10,6 +10,11 @@ const classicStandings = (state = intialState, action) => {
   switch (action.type) {
     case FETCH_DATA:
       return state;
+    case FETCH_DATA_REJECTED: {
+      // TODO: Toast message
+      console.log(action);
+      return state;
+    }
     case DATA_FETCHED:
       return {...state, data: action.payload};
     default:
