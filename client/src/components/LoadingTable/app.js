67e93a819cb90b8b865680c0fdd11ce3c813d52d
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2018-02-10 14:43:38 +0545

    Add Spinner to LoadingTable

--- client/src/components/LoadingTable/app.js
+++ client/src/components/LoadingTable/app.js
@@ -1,10 +1,12 @@
 // npm packages
 import React from 'react';
 import {string} from 'prop-types';
+import Loader from 'react-loader-spinner';
 
 const LoadingTable = ({title = ''}) => (
   <nav className="level">
     <div className="level-item has-text-centered">
+      <Loader type="Ball-Triangle" color="#00BFFF" height="50" width="50" />
       <h3>Loading... {title}</h3>
     </div>
   </nav>
