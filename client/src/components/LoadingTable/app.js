d2548932ab60be2ec664a5124d38c306666d01df
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2018-02-10 13:37:23 +0545

    Add title to LoadingTable

--- client/src/components/LoadingTable/app.js
+++ client/src/components/LoadingTable/app.js
@@ -1,11 +1,16 @@
+// npm packages
 import React from 'react';
+import {string} from 'prop-types';
 
-const LoadingTable = () => (
+const LoadingTable = ({title = ''}) => (
   <nav className="level">
     <div className="level-item has-text-centered">
-      <h3>Loading...</h3>
+      <h3>Loading... {title}</h3>
     </div>
   </nav>
 );
+LoadingTable.propTypes = {
+  title: string,
+};
 
 export default LoadingTable;
