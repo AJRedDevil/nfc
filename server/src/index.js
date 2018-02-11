f0b3a6d3714244c403097f3dc15df4362294bdc2
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2018-02-11 13:02:19 +0545

    Public access from docker

--- server/src/index.js
+++ server/src/index.js
@@ -12,8 +12,12 @@ fastify.use(require('cors')());
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen(3000);
-    fastify.log.info(`server listening on ${fastify.server.address().port}`);
+    await fastify.listen(3000, '0.0.0.0', err => {
+      if (err) {
+        fastify.log.error(err);
+        process.exit(1);
+      }
+    });
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
