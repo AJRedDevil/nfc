4c02857f114ed7ef1540badae3559fe07b3212a1
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2018-02-18 09:54:54 +0545

    applied css to table

--- client/src/components/Table/app.js
+++ client/src/components/Table/app.js
@@ -8,23 +8,11 @@ import TBody from './tbody';
 import TablePropTypes from './propTypes';
 
 const Table = ({head, body, foot}) => (
-  <section>
-    <div className="tile is-ancestor">
-      <div className="tile is-vertical">
-        <div className="tile">
-          <div className="tile is-parent is-vertical">
-            <article className="tile is-child notification is-primary">
-              <table className="table is-fullwidth">
-                <THead data={head} />
-                <TFoot data={foot} />
-                <TBody data={body} />
-              </table>
-            </article>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
+  <table className="table is-fullwidth is-striped is-narrow is-hoverable">
+    <THead data={head} />
+    <TFoot data={foot} />
+    <TBody data={body} />
+  </table>
 );
 Table.propTypes = {
   head: TablePropTypes.tableHead,
